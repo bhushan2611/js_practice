10df9737fc2990fa5dabec6ccf3b01cf09f77736
Bhushan <80047431+bhushan2611@users.noreply.github.com>
2024-01-22 13:20:07 +0000

    datatypes added

--- js_basics/start.js
+++ js_basics/start.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+// console.log("hello world");
+// alert("hello") // cannot used in nodeJs / can use only when js in html script tag
+
+// console.log(3+5);
+// datatypes : string,number,boolean,undefined,null,nan
+// null is a standalone value (type of null is object)
+// undefined when not assign any value (type of undefined is undefined)
+let name = 'bhushan'
+let 
